c5f93d0e75d55f15098c776336f2f6ec83c501bf
UvinduEshwarage <uvindu.nethmina30@gmail.com>
2026-05-06 18:34:44 +0530

    maked  changes in mongodb connected msg

--- isuru_fiberglass/lib/mongodb.ts
+++ isuru_fiberglass/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient, Db } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
+const uri = process.env.MONGODB_URL!;
 const dbName = process.env.MONGODB_DB!;
 
 if (!uri) {
@@ -11,15 +11,21 @@ if (!dbName) {
   throw new Error("Please define MONGODB_DB in .env");
 }
 
+
 let client: MongoClient;
 let db: Db;
 
 export async function connectDB(): Promise<Db> {
-  if (db) return db; // reuse connection
+  if (db) {
+    console.log("mongodb connected (reusing existing connection)");
+    return db;
+  }
 
+  console.log("Attempting to connect to MongoDB...");
   client = new MongoClient(uri);
   await client.connect();
 
   db = client.db(dbName);
+  console.log("mongodb connected successfully!");
   return db;
 }
